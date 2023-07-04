b9304da2d5dc74d125a1cad962cd701cb25e62d7
Sergii Marchenko <marikmarchelo@gmail.com>
2023-07-04 20:08:48 +0300

    add commit

--- src/index.js
+++ src/index.js
@@ -37,8 +37,8 @@ createMarkup(instruments);
 function findProduct(productId) {
   return instruments.find(({ id }) => id === productId);
 }
-const instance = basicLightbox.create(`
-	<h1>Dynamic Content</h1>
-	<p>You can set the content of the lightbox with JS.</p>
-`);
-instance.show();
+// const instance = basicLightbox.create(`
+// 	<h1>Dynamic Content</h1>
+// 	<p>You can set the content of the lightbox with JS.</p>
+// `);
+// instance.show();
